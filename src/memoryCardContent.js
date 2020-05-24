570c42f289d7718025ccc633a9419d3367a2b0d6
Jenny Poon <jenny.poon@live.ca>
2020-05-24 13:52:28 -0700

    refactor: generic card comp to map through

---
+++ src/memoryCardContent.js
@@ -0,0 +1,143 @@
+exports.cardContent = [
+  {
+    cardTitle: "Door Games",
+    cardContent:
+      "Here's a sneak peak of one of the games they had to go through! Extremely entertaining to watch! Shout out to the bridesmaid for coming up with such creative ideas! That BSB serenade was the cherry on top!",
+    cardMedia: [
+      {
+        mediaType: "gif",
+        mediaSrc: "banana_train",
+      },
+      {
+        mediaType: "img",
+        mediaSrc: "bsb-singing",
+      },
+    ],
+  },
+  {
+    cardTitle: "First Dance",
+    cardContent:
+      "Song: Lady Gaga - Is that Alright \n   Jenny is so proud of Stephen for taking dancing lessons! Not bad   for 3 dancing lessons! Thank you so much Derek for the live performance! My parents did a double take when they realized you were singing live!",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "first-dance",
+      },
+    ],
+  },
+  {
+    cardTitle: "Blindfold Musical Chairs",
+    cardContent:
+      "Thank you guys for being such good sports about playing Blindfold Musical Chairs! We didn't realize how competitive it could get but it was sure entertaining to watch!",
+    cardMedia: [
+      {
+        mediaType: "gif",
+        mediaSrc: "musical_chairs",
+      },
+      {
+        mediaType: "img",
+        mediaSrc: "musical-chairs-winner",
+      },
+    ],
+  },
+  {
+    cardTitle: "Our Awesome MCs",
+    cardContent:
+      "Thank you Derek and Elysia for being our MCs! You guys did an amazing job!",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "mcs",
+      },
+    ],
+  },
+  {
+    cardTitle: "Memories with Friends",
+    cardContent:
+      "Stephen worked hard on this slideshow and it was such a shame that the DJ couldn't figure out how to get the sound to play through his system. So here it is again! Turn up the volume and enjoy!",
+    mdSize: 7,
+    cardMedia: [
+      {
+        mediaType: "youtube",
+        mediaSrc: process.env.YOUTUBE_FRIEND_ID,
+      },
+    ],
+  },
+  {
+    cardTitle: "Chinese Karaoke",
+    cardContent:
+      "Shout out to Chris, Brian, and Ruzni for partipating in Chinese Karoke! You guys were such great sports about it and agreed to it as soon as we asked! Our relatives had so much fun guessing! Good work you three!",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "chinese-k",
+      },
+    ],
+  },
+  {
+    cardTitle: "Speeches",
+    cardContent:
+      "The speeches from our friends - it's going straight into the scrapbook!",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "chinese-k",
+      },
+    ],
+  },
+  {
+    cardTitle: "Mother/Father Dance",
+    cardContent: "Derek killing it during the Mother/Father Dance!",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "chinese-k",
+      },
+    ],
+  },
+  {
+    cardTitle: "Tea Ceremony",
+    cardContent:
+      "I never expected the tea ceremony to be so emotional. The family bond and love was so evident on this day. It was an honor to get a chance to serve tea to everyone on our family.",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "chinese-k",
+      },
+    ],
+  },
+  {
+    cardTitle: "Wedding Ceremony",
+    cardContent:
+      "Thank you Auntie Grace for being our wedding officiant. No words can describe how special this moment was for us.",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "ceremony-grace",
+      },
+    ],
+  },
+  {
+    cardTitle: "Photobooth",
+    cardContent:
+      "The photobooth was a huge hit! You can download your photobooth pictures up on the Links placed in the headers",
+    cardMedia: [
+      {
+        mediaType: "img",
+        mediaSrc: "ceremony-grace",
+      },
+    ],
+  },
+  {
+    cardTitle: "Memories with Family",
+    cardContent:
+      "It was fun watching our childhood videos on the VHS. The 90s were a blast! Kudos to our dads for carrying around a giant camcorder to capture these special moments.",
+    mdSize: 7,
+    cardMedia: [
+      {
+        mediaType: "youtube",
+        mediaSrc: process.env.YOUTUBE_FRIEND_ID,
+      },
+    ],
+  },
+]
--- src/pages/memories.js
+++ src/pages/memories.js
@@ -9,234 +9,73 @@ import Link from "@material-ui/core/Link"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { cardContent } from "../memoryCardContent"
 import Youtube from "../components/youtube"
-import bananaTrainGif from "../gifs/banana_train.gif"
 
-const FavMemories = () => {
+const cardComp = ({ cardTitle, cardContent, mdSize = 4, cardMedia }) => {
+  const cardMediaComp = (title, { mediaSrc, mediaType }) => {
+    if (mediaType === "youtube") {
+      return <Youtube youtubeId={mediaSrc} />
+    }
+    if (mediaType === "img") {
+      return (
+        <CardMedia
+          src={require(`../images/${mediaSrc}.jpg`)}
+          component="img"
+          title={title}
+        />
+      )
+    }
+    if (mediaType === "gif") {
+      return (
+        <CardMedia
+          src={require(`../gifs/${mediaSrc}.gif`)}
+          component="img"
+          title={title}
+        />
+      )
+    }
+  }
+
   return (
-    <Layout>
-      <SEO title="Favorite Memories" />
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Typography variant="h5">Our Favorite Memories</Typography>
-          <Typography variant="body1">
-            If you'd like to share the photos you've taken, make sure to upload
-            them{" "}
-            <Link
-              target="_blank"
-              rel="noreferrer"
-              href={process.env.WEBSITE_UPLOAD}
-            >
-              here!
-            </Link>
+    <Grid item md={mdSize} sm={12}>
+      <Card>
+        {cardMedia.map(media => cardMediaComp(cardTitle, media))}
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            {cardTitle}
           </Typography>
-        </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardMedia
-              src={bananaTrainGif}
-              component="img"
-              title="Door Games"
-            />
-            <CardMedia
-              src={require("../images/bsb-singing.jpg")}
-              component="img"
-              title="Door Games"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Door Games
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Here's a sneak peak of one of the games they had to go through!
-                Extremely entertaining to watch! Shout out to the bridesmaid for
-                coming up with such creative ideas! That BSB serenade was the
-                cherry on top!
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardMedia
-              src={require("../images/first-dance.jpg")}
-              component="img"
-              title="First Dance"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                First Dance
-              </Typography>
-              <Typography gutterBottom variant="body2">
-                Song: Lady Gaga - Is that Alright
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Jenny is so proud of Stephen for taking dancing lessons! Not bad
-                for 3 dancing lessons! Thank you so much Derek for the live
-                performance! My parents did a double take when they realized you
-                were singing live!
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardMedia
-              src={require("../gifs/musical_chairs.gif")}
-              component="img"
-              title="Blindfold Musical Chairs"
-            />
-            <CardMedia
-              src={require("../images/musical-chairs-winner.jpg")}
-              component="img"
-              title="Blindfold Musical Chairs"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                Blindfold Musical Chairs
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Thank you guys for being such good sports about playing
-                Blindfold Musical Chairs! We didn't realize how competitive it
-                could get but it was sure entertaining to watch!
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={4}>
-          <Card>
-            <CardMedia
-              src={require("../images/mcs.jpg")}
-              component="img"
-              title="Our Awesome MCs"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                Our Awesome MCs
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                Thank you Derek and Elysia for being our MCs! You guys did an
-                amazing job!
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item md={7} sm={12}>
-          <Card>
-            <CardContent>
-              <Youtube youtubeId={process.env.YOUTUBE_FRIEND_ID} />
-              <br />
-              <Typography gutterBottom variant="h5">
-                Memories with Friends
-              </Typography>
-              <Typography variant="body2" component="p">
-                Stephen worked hard on this slideshow and it was such a shame
-                that the DJ couldn't figure out how to get the sound to play
-                through his system. So here it is again! Turn up the volume and
-                enjoy!
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item md={4} xs={12}>
-          <Card>
-            <CardMedia
-              src={require("../images/chinese-k.jpg")}
-              component="img"
-              title="Reception Game"
-            />
-
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                Chinese Karaoke
-              </Typography>
-              <Typography variant="body2" component="p">
-                Shout out to Chris, Brian, and Ruzni for partipating in Chinese
-                Karoke! You guys were such great sports about it and agreed to
-                it as soon as we asked! Our relatives had so much fun guessing!
-                Good work you three!
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-
-        <Grid item md={4} sm={12}>
-          <Typography variant="body1" paragraph={true}>
-            The speeches from our friends - it's going straight into the
-            scrapbook!
+          <Typography variant="body2" component="p">
+            {cardContent}
           </Typography>
-        </Grid>
-        <Grid item md={4} sm={12}>
-          <Typography variant="body1" paragraph={true}>
-            Derek killing it during the Mother/Father Dance!
-          </Typography>
-        </Grid>
-        <Grid item md={4} sm={12}>
-          <Card>
-            <CardMedia
-              src={require("../images/tea-ceremony.jpg")}
-              component="img"
-              title="Ceremony"
-            />
-
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                Tea Ceremony
-              </Typography>
-              <Typography variant="body2" component="p">
-                I never expected the tea ceremony to be so emotional. The family
-                bond and love was so evident on this day. It was an honor to get
-                a chance to serve tea to everyone on our family.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item md={4} sm={12}>
-          <Card>
-            <CardMedia
-              src={require("../images/ceremony-grace.jpg")}
-              component="img"
-              title="Ceremony"
-            />
-
-            <CardContent>
-              <Typography gutterBottom variant="h5">
-                Wedding Ceremony
-              </Typography>
-              <Typography variant="body2" component="p">
-                Thank you Auntie Grace for being our wedding officiant. No words
-                can describe how special this moment was for us.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item md={4} sm={12}>
-          <Typography variant="body1" paragraph={true}>
-            The photobooth was a huge hit! You can download your photobooth
-            pictures up on the Links placed in the headers
-          </Typography>
-        </Grid>
-        <Grid item md={7} sm={12}>
-          <Card>
-            <CardContent>
-              <Youtube youtubeId={process.env.YOUTUBE_FRIEND_ID} />
-              <br />
-              <Typography gutterBottom variant="h5">
-                Memories with Family
-              </Typography>
-              <Typography variant="body2" component="p">
-                It was fun watching our childhood videos on the VHS. The 90s
-                were a blast! Kudos to our dads for carrying around a giant
-                camcorder to capture these special moments.
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-      <GatsbyLink to="/">Go back to the homepage</GatsbyLink>
-    </Layout>
+        </CardContent>
+      </Card>
+    </Grid>
   )
 }
+
+const FavMemories = () => (
+  <Layout>
+    <SEO title="Favorite Memories" />
+    <Grid container spacing={3}>
+      <Grid item xs={12}>
+        <Typography variant="h5">Our Favorite Memories</Typography>
+        <Typography variant="body1">
+          If you'd like to share the photos you've taken, make sure to upload
+          them{" "}
+          <Link
+            target="_blank"
+            rel="noreferrer"
+            href={process.env.WEBSITE_UPLOAD}
+          >
+            here!
+          </Link>
+        </Typography>
+      </Grid>
+      {cardContent.map(moment => cardComp(moment))}
+    </Grid>
+    <GatsbyLink to="/">Go back to the homepage</GatsbyLink>
+  </Layout>
+)
 
 export default FavMemories
